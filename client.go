81b750c8edea37b139c672a0478375fda4f86f78
konojunya <junya03dance@gmail.com>
2017-04-19 00:21:26 +0900

    サーバーかききった

---
+++ client.go
@@ -0,0 +1,122 @@
+package main
+
+import(
+  "bytes"
+  "log"
+  "net/http"
+  "time"
+
+  "github.com/gorilla/websocket"
+)
+
+const (
+  // 他の人にメッセージを書き込むことができる時間
+  WRITE_WAIT = 10 * time.Second
+
+  // 仲間からメッセージを読み取ることができる時間
+  PONG_WAIT = 60 * time.Second
+
+  // この時間で仲間にピアリングを送信する。注意としてPONG_WAITより小さい必要がある
+  PING_PERIOD = (PONG_WAIT * 9) / 10
+
+  // 仲間から許容できる最大のサイズ
+  MAX_MESSAGE_SIZE = 512
+)
+
+var (
+  newline = []byte{"\n"}
+  space = []byte{" "}
+)
+
+var upgrader = websocket.Upgrader{
+  ReadBufferSize: 1024,
+  WriteBufferSize: 1024,
+}
+
+type Client struct {
+  hub *Hub
+
+  // websocketのコネクション
+  conn *websocket.Conn
+
+  // バッファされた受信メッセージのチャンネル
+  send chan []byte
+}
+
+func (c *Client) readPump(){
+  defer func(){
+    c.hub.unregister <- c
+    c.conn.Close()
+  }()
+
+  c.conn.SetReadLimit(MAX_MESSAGE_SIZE)
+  c.conn.SetReadDeadline(time.Now().Add(PONG_WAIT))
+  c.conn.SetPongHandler(func(string) error {c.conn.SetReadDeadline(time.Now().Add(PONG_WAIT)); return nil})
+  for {
+    _, message, err := c.conn.ReadMessage()
+    if err != nil {
+      if websocket.IsUnexpectedCloseError(err,websocket.CloseGoingAway){
+        log.Printf("error: %v",err)
+      }
+      break
+    }
+
+    message = bytes.TrimSpace(bytes.Replace(message,newline, space, -1))
+    c.hub.broadcast <- message
+  }
+}
+
+func (c *Client) writePump() {
+  ticker := time.NewTicker(pingPeriod)
+  defer func(){
+    ticker.Stop()
+    c.conn.Close()
+  }()
+
+  for {
+    select {
+    case message, ok := <-c.send:
+      c.conn.SetReadDeadline(time.Now().Add(WRITE_WAIT))
+
+      if !ok {
+        c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+        return
+      }
+
+      w, err := c.conn.NextWriter(websocket.TextMessage)
+      if err != nil {
+        return
+      }
+      w.Write(message)
+
+      n := len(c.send)
+      for i := 0; i < n; i++ {
+        w.Write(newline)
+        w.Write(<-c.send)
+      }
+
+      if err := w.Close(); err != nil {
+        return
+      }
+
+    case <-ticker.C:
+      c.conn.SetWriteDeadline(time.Now().Add(WRITE_WAIT))
+      if err := c.conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+        return
+      }
+    }
+  }
+}
+
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request){
+  conn, err := upgrader.Upgrade(w,r,nil)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+
+  client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+  client.hub.register <- client
+  go client.writePump()
+  client.readPump()
+}
